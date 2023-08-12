9d1c5b37fcfe81ec57481ee02df1f3798a95cd20
Reisir <reisirito@gmail.com>
2023-08-13 01:36:51 +0300

    Update sites.js

--- scripts/sites.js
+++ scripts/sites.js
@@ -1,10 +1,8 @@
+// Location of the webring landing page
+export const webringUrl = "https://bentovid.com/webring"
+
 // Array of sites in the webring
 export const sites = [
-  // {
-  //   title: "BentoVid Webring",
-  //   url: "https://bentovid.com/webring",
-  //   description: "The homepage of the BentoVid Webring",
-  // },
   {
     title: "BentoVid.com",
     url: "https://bentovid.com/",
@@ -15,11 +13,6 @@ export const sites = [
     url: "https://amv.tools/",
     description: "Editing guides and resources (for AE & Vegas Pro)",
   },
-  // {
-  //   title: "Vidders Corner",
-  //   url: "https://vidderscorner.wordpress.com/",
-  //   description: "Falconone's AMV blog",
-  // },
   {
     title: "Synæsthesia Productions",
     url: "https://synaesthesia.neocities.org/",
