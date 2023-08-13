b201df02b2859574b3985efd3aba85bd91605778
reisir <reisirito@gmail.com>
2023-08-13 19:40:11 +0300

    prefer current site

--- scripts/common.js
+++ scripts/common.js
@@ -12,18 +12,18 @@ export const localParams = localURL.searchParams;
 
 // www. pattern
 export const www = /^www\./im;
+const noWWW = (s) => new URL(s).hostname.replace(www, "");
+export const currentHost = noWWW(window.location);
 
 // Get the referrer hostname (or current page)
-export const referrer = (() => {
-  const r = document.referrer || document.location;
-  if (!r) return;
-  else return new URL(r).hostname.replace(www, "");
-})();
+export const referrer = (() => noWWW(document.referrer || document.location))();
 
 // Find the current site from the sites array
-export const currentIndex = sitesArr.findIndex(
-  (s) => new URL(s.url).hostname.replace(www, "") === referrer
-);
+export const currentIndex = (() => {
+  let ci = sitesArr.findIndex(({ url }) => noWWW(url) === currentHost);
+  if (ci != -1) return ci;
+  return sitesArr.findIndex(({ url }) => noWWW(url) === referrer);
+})();
 
 // sites is a global from ./sites.js
 // Here we monkeywrench a bunch of stuff to the array for ease of use
@@ -36,8 +36,6 @@ sitesArr.current = sitesArr[currentIndex];
 sitesArr.previous = (i = currentIndex) => sitesArr.circular(i - 1);
 sitesArr.next = (i = currentIndex) => sitesArr.circular(i + 1);
 
-const noWWW = (s) => new URL(s).hostname.replace(www, "");
-export const currentHost = noWWW(window.location);
 sitesArr.random = (i = currentIndex) => {
   const otherSites = sitesArr.filter((s) => noWWW(s.url) !== currentHost);
   return otherSites[Math.floor(otherSites.length * Math.random())];
